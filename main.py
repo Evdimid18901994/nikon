f0f755f459cb101c2c3ce75b77304525fde56a3e
Evdimid18901994 <99124478+Evdimid18901994@users.noreply.github.com>
2025-03-17 15:43:51 +0300

    the first

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
 
-page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+st.set_page_config(initial_sidebar_state="collapsed")
+
+pages = ["Home", "Library", "Tutorials", "Development", "Download"]
+styles = {
+    "nav": {
+        "background-color": "blue",
+    },
+    "div": {
+        "max-width": "32rem",
+    },
+    "span": {
+        "color": "white !important" ,
+        "padding": "14px",
+    },
+    "active": {
+        "background-color": "white",
+        "color": "var(--text-color)",
+        "font-weight": "normal",
+        "padding": "14px",
+    },
+    "hover": {
+        "background-color": "rgba(255, 255, 255, 0.35)",
+    },
+}
+
+page = st_navbar(pages, styles=styles)
 st.write(page)
 
+with st.sidebar:
+    st.write("Sidebar")
